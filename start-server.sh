42aa2f70dcd1491f6d8d11186614c92f7c43c6ca
Guilherme Baptista <guisgb13@gmail.com>
2015-03-20 06:50:39 +0000

    Start-server doesn't need to know local IP assigned to the machine

--- start-server.sh
+++ start-server.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-
-IP="127.0.0.1"
-PORT = 3031
-
-uwsgi --http-socket $IP:$PORT --wsgi-file server.py --callable app --enable-threads
+uwsgi --http-socket 0.0.0.0:3031 --wsgi-file server.py --callable app --enable-threads
