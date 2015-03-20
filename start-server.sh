cc9e4f03958496319ffd51b4f90e791aa9440069
Guilherme Baptista <guisgb13@gmail.com>
2015-03-20 07:36:21 +0000

    Easily use a different PORT

--- start-server.sh
+++ start-server.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-uwsgi --http-socket 0.0.0.0:3031 --wsgi-file server.py --callable app --enable-threads
+PORT=3032
+export PORT=$PORT
+uwsgi --http-socket 0.0.0.0:$PORT --wsgi-file server.py --callable app --enable-threads
